a70ea1ec5ddb8b92a03d24bf71e3d476e2df7c63
Jack Moxley <jackokmoxley@gmail.com>
2022-08-31 02:31:18 +0100

    Changed the extruder rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1073,7 +1073,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 142 }
 
 /**
  * Default Max Feed Rate (mm/s)
